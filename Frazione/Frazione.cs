8be827d460edd27aca8321ce0b9709af2aebeeb7
andreaRoli <aggiustatuttobob4@gmail.com>
2024-01-31 18:00:36 +0100

    Aggiungere i file di progetto.

---
+++ Frazione/Frazione.cs
@@ -0,0 +1,52 @@
+﻿namespace Frazione
+{
+    internal class Frazione
+    {
+        public Frazione(int numeratore, int denominatore)
+        {
+            Numeratore = numeratore;
+            Denominatore = denominatore;
+        }
+
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+
+      public void Semplifica (ref int numeratore, ref int denominatore)
+        {
+            int mcd=MCD(numeratore,denominatore);
+
+            numeratore /= mcd;
+            denominatore /= mcd;
+
+
+        }
+         public int MCD(int a, int b)
+        {
+            int resto;
+
+            while (b != 0)
+            {
+                resto = a % b;
+               a =b;
+                b = resto;
+            }
+            return a;
+        }
+
+        public static Frazione Parse(string s)
+        {
+            string[] NumDen = s.Split('/');
+            if (NumDen.Length == 2 && int.TryParse(NumDen[0], out int numeratore ) && int.TryParse(NumDen[1], out int denominatore))
+            {
+
+                return new Frazione(numeratore , denominatore);
+            }
+            else { return null;  }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} / {1}", Numeratore, Denominatore);
+        }
+    }
+}
---
+++ Frazione/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Frazione f = Frazione.Parse("16/12");
+            int Num = 16;
+            int Den = 12;
+
+            f.Semplifica(ref Num, ref Den);
+
+            Console.WriteLine("La frazione semplificata è {0}", f.ToString());
+            Console.ReadLine();
+        }
+    }
+}
